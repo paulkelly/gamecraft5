f21b394d96be4135aac0a76f2534fece1708a035
Seán Mac Dónaill <johnmcdonnell89@gmail.com>
2014-05-17 17:55:43 +0100

    added popper prefab and game monitor

---
+++ Assets/Scripts/GameMonitor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMonitor : MonoBehaviour {
+
+	public GameObject player;
+	GameObject[] players;
+	bool[] deaths;
+	public int numPlayers = 4;
+	public int numDeaths = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		players = new GameObject[numPlayers];
+		deaths = new bool[numPlayers];
+		for (int i = 0; i < numPlayers; i++)
+		{
+			players[i] = (GameObject)Instantiate(player);
+			deaths[i] = false;
+		}
+	}
+
+	public void pop(int playerNum)
+	{
+		deaths[playerNum] = true;
+		numDeaths++;
+		Debug.Log(playerNum);
+		/*if (numDeaths >= numPlayers - 1)
+		{
+			Debug.Log(playerNum);
+		}*/
+	}
+
+}
--- Assets/Scripts/Pop.cs
+++ Assets/Scripts/Pop.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class Pop : MonoBehaviour {
 
+	public GameMonitor gameMonitor;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
+			FanController fan = other.gameObject.GetComponent<FanController>();
+			gameMonitor.pop(fan.playerNum);
 			Destroy(other.gameObject);
 		}
 	}
