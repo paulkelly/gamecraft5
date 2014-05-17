82b3dcf394060c5921e3f42d115002d807cf2443
Paul Kelly <pkelly429@gmail.com>
2014-05-17 15:56:24 +0100

    Fixing tags and adding movement scripts.

---
+++ Assets/Scripts/BalloonMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonMovement : MonoBehaviour
+{
+	float fanRadius = 0.2f;
+	Quaternion fanAngle = Quaternion.identity;
+
+	GameObject fan;
+
+	// Use this for initialization
+	void Start ()
+	{
+		fan = gameObject.transform.FindChild ("Fan").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Move(float h, float v)
+	{
+
+	}
+}
---
+++ Assets/Scripts/FanController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BalloonMovement))]
+public class FanController : MonoBehaviour {
+
+	public int playerNum = 1;
+
+	private BalloonMovement balloon;
+
+	void Start()
+	{
+		balloon = GetComponent<BalloonMovement>();
+	}
+	
+	void FixedUpdate ()
+	{
+		#if CROSS_PLATFORM_INPUT
+		float h = CrossPlatformInput.GetAxis("Horizontal");
+		float v = CrossPlatformInput.GetAxis("Vertical");
+		#else
+		float h = Input.GetAxis("P" + playerNum + "Horizontal");
+		float v = Input.GetAxis("P" + playerNum + "Vertical");
+		#endif
+
+		balloon.Move (h, v);
+
+	}
+}
