eafb85dfb39bb6381495d6ce353d688c9aaa8685
Seán Mac Dónaill <johnmcdonnell89@gmail.com>
2014-05-17 16:05:54 +0100

    added new scenes and popper object

---
+++ Assets/Scripts/Pop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pop : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
